73d161d88632c120ad493df1b83230a93ff95da9
Callou <bouvryalexis@gmail.com>
2023-10-25 23:49:44 +0200

    Initial Commit

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -10,14 +10,14 @@ const PostDetails = () => {
             <Navbar />
             <div className='px-8 md:px-[200px] mt-8'>
                 <div className='flex justify-between items-center'>
-                    <h1 className='text-2xl font-bold text-black md:text-3xl'>Message text test</h1>
+                    <h1 className='text-2xl font-bold text-black md:text-3xl'>Title text test</h1>
                     <div className='flex items-center justify-center space-x-2'>
                         <p><PiNotePencilBold /></p>
                         <p><BsFillTrashFill /></p>
                     </div>
                 </div>
                 <div className='flex items-center justify-between mt-2 md:mt-4'>
-                    <p>@callou</p>
+                    <p>@username</p>
                     <div className='flex space-x-2'>
                         <p>01/01/2023</p>
                         <p>15:02</p>
@@ -32,8 +32,32 @@ const PostDetails = () => {
                         <div className='bg-gray-300 rounded-lg px-3 py-1'>Categorie 2</div>
                     </div>
                 </div>
-                <div>
-                    <div className=''></div>
+                <div className='flex flex-col mt-4'>
+                    <h3 className='mt-6 mb-4 font-semibold'>Comments:</h3>
+                    <div className='px-2 py-2 mt-2 bg-gray-200 rounded-lg my-2'>
+                        <div className='flex items-center justify-between'>
+                            <h3 className='font-bold text-gray-600'>pseudo</h3>
+                            <div className='flex justify-center items-center space-x-4'>
+                                <p className='text-gray-500 text-sm'>date</p>
+                                <p className='text-gray-500 text-sm'>heure</p>
+                                <p className='text-gray-600'><PiNotePencilBold /></p>
+                                <p className='text-gray-600'><BsFillTrashFill /></p>
+                            </div>
+                        </div>
+                        <p className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                    <div className='px-2 py-4 mt-2 bg-gray-200 rounded-lg my-2'>
+                        <div className='flex items-center justify-between'>
+                            <h3 className='font-bold text-gray-600'>pseudo</h3>
+                            <div className='flex justify-center items-center space-x-4'>
+                                <p className='text-gray-500 text-sm'>date</p>
+                                <p className='text-gray-500 text-sm'>heure</p>
+                                <p className='text-gray-600'><PiNotePencilBold /></p>
+                                <p className='text-gray-600'><BsFillTrashFill /></p>
+                            </div>
+                        </div>
+                        <p className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
                 </div>
             </div>
             <Footer />
